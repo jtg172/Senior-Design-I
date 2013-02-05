bbc0e2571c8a751b7b35c27d0d62846ee94538fe
Justin <jtg172@msstate.edu>
2013-02-04 20:09:39 -0600

    added comments and cleaned up code, updated for analog sticks mapping, added serial debug

--- DC_Motor_HBridge/DC_Motor_HBridge.ino
+++ DC_Motor_HBridge/DC_Motor_HBridge.ino
@@ -1,9 +1,8 @@
 /*************************************************************
-Motor Shield 2-Channel DC Motor Demo
-by Randy Sarafan
+H-Bridge DC Motor Controller
 
-For more information see:
-http://www.instructables.com/id/Arduino-Motor-Shield-Tutorial/
+Robbie Lundine
+Justin Gilmer
 
 *************************************************************/
 //Including Xbox USB libraries
@@ -13,21 +12,14 @@ USB Usb;
 XBOXUSB Xbox(&Usb);
 
 //Define ports
-
-int M1_1 = 3;
-int M1_2 = 5;
-int M2_1 = 6;
-int M2_2 = 9;
-
-//char CL_UD = A0;
-//char CL_RL = A1;
-//
-//char CR_UD = A2;
-//char CR_RL = A3;
+int M1_1 = 5; //left //3
+int M1_2 = 9; //5
+int M2_1 = 6; //right //6
+int M2_2 = 10; //9
 
 void setup() {
-//  Serial.begin(9600);
-  Serial.begin(115200);
+  Serial.begin(9600);
+//  Serial.begin(115200);
 
   if (Usb.Init() == -1) {
     Serial.print(F("\r\nOSC did not start"));
@@ -43,69 +35,79 @@ void setup() {
   pinMode(M2_1, OUTPUT);
   pinMode(M2_2, OUTPUT);
   
-   Xbox.setLedMode(ALTERNATING);
-  //Setup Controller
-//  pinMode(CL_UD, INPUT);
-//  pinMode(CR_UD, INPUT);
+  Xbox.setLedMode(ALTERNATING);
+   
+  //TCCR0B=0x02;
+  //TCCR1B=0x02;
+  //TCCR2B=0x02;
 }
 
 void loop(){
   Usb.Task();
   int M1_1_S, M1_2_S, M2_1_S, M2_2_S;
-  int CL_UD_S, CR_UD_S;
+  int CR_RL_S, CR_UD_S;
   
   M1_1_S = M1_2_S = M2_1_S = M2_2_S = 0;
-  CL_UD_S = CR_UD_S = 0;
+  CR_RL_S = CR_UD_S = 0;
   
   CR_UD_S = Xbox.getAnalogHat(RightHatY);
-//  CR_UD_S = getAnalog(LeftHatY);
+  CR_RL_S = Xbox.getAnalogHat(RightHatX);
   
-  /*
-  Serial.print("CR_UD_S = ");
-  Serial.print(CR_UD_S, DEC);
-  delay(300);
- 
-  Serial.print(", CL_UD_S = ");
-  Serial.print(CL_UD_S, DEC);
-  delay(1000);
-  */
-   
- if(CR_UD_S > 100)
+  ///* 
+  if(CR_UD_S > 800)
   {
-     M1_1_S = map(CR_UD_S, 0, 32767, 0, 255) ;
-     M1_2_S = LOW;
-     M2_1_S = map(CR_UD_S, 0, 32767, 0, 255) ;
-     M2_2_S = LOW;
+     // M1 FROWARD, M2 FORWARD
+     M1_1_S = M2_1_S = map(CR_UD_S, 801, 32767, 0, 255);
+     M1_2_S = M2_2_S = LOW;
+
   } 
-  else if(CR_UD_S <= -100)
+  else if( CR_UD_S < (-800) )
   {
-    CR_UD_S = CR_UD_S * (-1);
-    M1_1_S = LOW;
-    M1_2_S = map(CR_UD_S, 0, 32767, 255, 0);
-    M2_1_S = LOW;
-    M2_2_S = map(CL_UD_S, 0, 521, 255, 0);
+     //M1 REVERSE, M2 REVERSE
+     M1_1_S = M2_1_S = LOW;
+     M1_2_S = M2_2_S = map(CR_UD_S, -799, -32768, 0, 255);
   }
+  //*/
+  ///*
+  if(CR_RL_S > 800)
+  {
+     //M1 REVERSE, M2 FORWARD
+     M1_1_S = M2_2_S = LOW;
+     M1_2_S = M2_1_S = map(CR_RL_S, 801, 32767, 0, 255);
+  }
+  else if(CR_RL_S < (-800) )
+  {
+     //M1 FORWARD, M2 REVERSE
+     M1_1_S = M2_2_S = map(CR_RL_S, -799, -32768, 0, 255);
+     M1_2_S = M2_1_S = LOW;
+  }
+  //*/
+  /*
+  Serial.print("M1_1_S=");
+  Serial.print(M1_1_S, DEC);
+  Serial.print(", M1_2_S=");
+  Serial.print(M1_2_S, DEC);
+  Serial.print("\n");
   
-//  if(CL_UD_S > 521)
-//  {
-//    CL_UD_S = CL_UD_S - 521;
-//    M2_1_S = map(CL_UD_S, 0, 502, 0, 255);
-//    M2_2_S = LOW;
-//  } 
-//  else if(CL_UD_S <= 521)
-//  {
-//    M2_1_S = LOW;
-//    M2_2_S = map(CL_UD_S, 0, 521, 255, 0);
-//  }
- 
+  Serial.print("M2_1_S=");
+  Serial.print(M2_1_S, DEC);
+  Serial.print(", M2_2_S=");
+  Serial.print(M2_2_S, DEC);
+  Serial.print("\n");
+  
+  Serial.print("CR_UD_S=");
+  Serial.print(CR_UD_S, DEC);
+  Serial.print("\n\n");
+  delay(300);
+  */
 
   //Set Motor 1 
-  analogWrite(M1_1, M1_1_S); //Establishes forward direction of Channel A
-  analogWrite(M1_2, M1_2_S);   //Disengage the Brake for Channel A
+  analogWrite(M1_1, M1_1_S);
+  analogWrite(M1_2, M1_2_S);
 
   //Set Motor 2
-  analogWrite(M2_1, M2_1_S);  //Establishes backward direction of Channel B
-  analogWrite(M2_2, M2_2_S);   //Disengage the Brake for Channel B
+  analogWrite(M2_1, M2_1_S);
+  analogWrite(M2_2, M2_2_S);
 
   /*
   //Set Motor 1 test
