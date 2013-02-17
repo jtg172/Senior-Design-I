ac6d71bdddf4c090f25f8c999fecc9aabc846f12
Justin <jtg172@msstate.edu>
2013-02-17 15:43:53 -0600

    updated for 5 motors and xbox triggers

--- DC_Motor_HBridge/DC_Motor_HBridge.ino
+++ DC_Motor_HBridge/DC_Motor_HBridge.ino
@@ -12,10 +12,16 @@ USB Usb;
 XBOXUSB Xbox(&Usb);
 
 //Define ports
-int M1_1 = 5; //left //3
-int M1_2 = 9; //5
-int M2_1 = 6; //right //6
-int M2_2 = 10; //9
+int M1_1 = 2; //left
+int M1_2 = 3; 
+int M2_1 = 4; //right
+int M2_2 = 5; 
+int M3_1 = 6; //bottom1
+int M3_2 = 7; 
+int M4_1 = 8; //bottom2
+int M4_2 = 9; 
+int M5_1 = 10; //bottom3
+int M5_2 = 11; 
 
 void setup() {
   Serial.begin(9600);
@@ -35,6 +41,18 @@ void setup() {
   pinMode(M2_1, OUTPUT);
   pinMode(M2_2, OUTPUT);
   
+  //Setup Motor 3
+  pinMode(M3_1, OUTPUT);
+  pinMode(M3_2, OUTPUT);
+  
+  //Setup Motor 4
+  pinMode(M4_1, OUTPUT);
+  pinMode(M4_2, OUTPUT);
+  
+  //Setup Motor 5
+  pinMode(M5_1, OUTPUT);
+  pinMode(M5_2, OUTPUT);
+  
   Xbox.setLedMode(ALTERNATING);
    
   //TCCR0B=0x02;
@@ -44,16 +62,18 @@ void setup() {
 
 void loop(){
   Usb.Task();
-  int M1_1_S, M1_2_S, M2_1_S, M2_2_S;
-  int CR_RL_S, CR_UD_S;
+  int M1_1_S, M1_2_S, M2_1_S, M2_2_S, M3_1_S, M3_2_S, M4_1_S, M4_2_S, M5_1_S, M5_2_S;
+  int CR_RL_S, CR_UD_S, LT_S, RT_S;
   
-  M1_1_S = M1_2_S = M2_1_S = M2_2_S = 0;
-  CR_RL_S = CR_UD_S = 0;
+  M1_1_S = M1_2_S = M2_1_S = M2_2_S = M3_1_S = M3_2_S = M4_1_S = M4_2_S = M5_1_S = M5_2_S = 0;
+  CR_RL_S = CR_UD_S = LT_S = RT_S = 0;
   
   CR_UD_S = Xbox.getAnalogHat(RightHatY);
   CR_RL_S = Xbox.getAnalogHat(RightHatX);
   
-  ///* 
+  LT_S = Xbox.getButton(L2);
+  RT_S = Xbox.getButton(R2);
+  
   if(CR_UD_S > 800)
   {
      // M1 FROWARD, M2 FORWARD
@@ -67,8 +87,7 @@ void loop(){
      M1_1_S = M2_1_S = LOW;
      M1_2_S = M2_2_S = map(CR_UD_S, -799, -32768, 0, 255);
   }
-  //*/
-  ///*
+
   if(CR_RL_S > 800)
   {
      //M1 REVERSE, M2 FORWARD
@@ -81,8 +100,23 @@ void loop(){
      M1_1_S = M2_2_S = map(CR_RL_S, -799, -32768, 0, 255);
      M1_2_S = M2_1_S = LOW;
   }
-  //*/
+  
+  if(LT_S > 0)
+  {
+     M3_1_S = M4_1_S = M5_1_S = LOW;
+     M3_2_S = M4_2_S = M5_2_S = LT_S;
+  }
+  else if (RT_S > 0)
+  {
+     M3_1_S = M4_1_S = M5_1_S = RT_S;
+     M3_2_S = M4_2_S = M5_2_S = LOW;
+  }
+  
   /*
+  Serial.print("LT_S=");
+  Serial.print(LT_S, DEC);
+  //delay(300);
+  
   Serial.print("M1_1_S=");
   Serial.print(M1_1_S, DEC);
   Serial.print(", M1_2_S=");
@@ -108,6 +142,18 @@ void loop(){
   //Set Motor 2
   analogWrite(M2_1, M2_1_S);
   analogWrite(M2_2, M2_2_S);
+  
+  //Set Motor 3
+  analogWrite(M3_1, M3_1_S);
+  analogWrite(M3_2, M3_2_S);
+  
+  //Set Motor 2
+  analogWrite(M4_1, M4_1_S);
+  analogWrite(M4_2, M4_2_S);
+  
+  //Set Motor 2
+  analogWrite(M5_1, M5_1_S);
+  analogWrite(M5_2, M5_2_S);
 
   /*
   //Set Motor 1 test
